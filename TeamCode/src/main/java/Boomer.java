3cb85e830c9e31fde10bb73d7211348cb6cd02cf
Gabert205 <gaunderwood1205@gmail.com>
2020-01-27 16:02:58 -0600

    I don't remember what I changed

--- TeamCode/src/main/java/Boomer.java
+++ TeamCode/src/main/java/Boomer.java
@@ -7,8 +7,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Boomer extends OpMode {
 
-    private double x, x2, y, y2, power, power2, aliPower, liftPower;//Instantiating the vars that will be used for the power and direction of the DcMotors
-    private DcMotor frontRight, frontLeft, backRight, backLeft, ali, elevator;//Instantiating the DcMotors for the
+    private double x, x2, y, y2, power, power2, liftPower, hardWareSucksPower;//Instantiating the vars that will be used for the power and direction of the DcMotors
+    private DcMotor frontRight, frontLeft, backRight, backLeft, elevator, hardwareSucks;//Instantiating the DcMotors for the
     private Servo leftArm, rightArm, claw;
     private int count, clawCount;
     private boolean armsOpen, clawUp;
@@ -21,20 +21,20 @@ public class Boomer extends OpMode {
         frontLeft = hardwareMap.dcMotor.get("FL");//0 - hub2
         backRight = hardwareMap.dcMotor.get("BR");//2 - hub2
         backLeft = hardwareMap.dcMotor.get("BL");//3 - hub2
-        ali = hardwareMap.dcMotor.get("ALI");//0 - hub1
         elevator = hardwareMap.dcMotor.get("LIFT");//2 - hub1
         leftArm = hardwareMap.servo.get("LEFTARM");//0 - hub1
         rightArm = hardwareMap.servo.get("RIGHTARM");//1 - hub1
         claw = hardwareMap.servo.get("CLAW");//3 - hub1
+        hardwareSucks = hardwareMap.dcMotor.get("HARDWARESUCKS");//0 - hub1
 
         //Setting the x and y vars to the default value of 0.0
         x = 0.0;
         y = 0.0;
-        aliPower = 0;
         liftPower = 0;
+        hardWareSucksPower = 0;
 
         //Starting the claw at the up position
-        claw.setPosition(1);
+        claw.setPosition(.6);
         clawUp = false;
 
         //Setting the other vars that will be needed for the collection system to their default values
@@ -83,13 +83,9 @@ public class Boomer extends OpMode {
             power2 /= 3;
         }
 
-        aliPower = 0;
-        aliPower += gamepad1.right_trigger;
-        aliPower -= gamepad1.left_trigger;
-
         liftPower = 0;
-        if(gamepad1.left_bumper) liftPower += .5;
-        if(gamepad1.right_bumper) liftPower -= .5;
+        if(gamepad1.left_bumper) liftPower = 1;
+        if(gamepad1.right_bumper) liftPower = -1;
 
         //Count makes sure that the 'a' button can be held down and the servos will not spas out
         if(count < 0) count = 0;
@@ -133,6 +129,12 @@ public class Boomer extends OpMode {
         }
         else claw.setPosition(1);
 
+        hardWareSucksPower = 0;
+        hardWareSucksPower += gamepad1.right_trigger;
+        hardWareSucksPower -= gamepad1.left_trigger;
+
+        hardwareSucks.setPower(hardWareSucksPower);
+
         //Setting the different motors to their respective power for lateral movement
         frontLeft.setPower(power * ( x - y ) );
         frontRight.setPower(power * ( x + y ) );
@@ -145,7 +147,6 @@ public class Boomer extends OpMode {
         backLeft.setPower(power2 * -x2);
         backRight.setPower(power2 * -x2);
 
-        ali.setPower(aliPower);
         elevator.setPower(liftPower);
 
         //region Telemetry Data
@@ -155,7 +156,6 @@ public class Boomer extends OpMode {
         telemetry.addData("FR PWR", power * ( x + y ) );
         telemetry.addData("BL PWR", power * ( - x - y ) );
         telemetry.addData("BR PWR", power * ( - x + y ) );
-        telemetry.addData("ALI PWR", aliPower );
         telemetry.addData("ELEVATOR PWR", liftPower);
         telemetry.addData("CLAW UP", clawUp);
         //endregion
